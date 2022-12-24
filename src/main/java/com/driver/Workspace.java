949ced091ee043eb84206c18b8284c9c821276fc
SHARGINJAY PATEL <sharginjaypatel12021999@gmail.com>
2022-12-24 14:06:01 +0530

    done

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -28,24 +28,36 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-//        ArrayList<Pair<LocalTime, Integer>> events = new ArrayList<>();
-//        for (Meeting meeting : calendar) {
-//            events.add(Pair.of(meeting.getStartTime(), 1));
-//            events.add(Pair.of(meeting.getEndTime(), -1));
-//        }
-//        Collections.sort(events, (a, b) -> {
-//            int cmp = a.getKey().compareTo(b.getKey());
-//            if (cmp == 0) {
-//                cmp = a.getValue().compareTo(b.getValue());
-//            }
-//            return cmp;
-//        });
+        ArrayList<Pair<LocalTime, Integer>> events = new ArrayList<>();
+        for (Meeting meeting : calendar) {
+            events.add(Pair.of(meeting.getStartTime(), 1));
+            events.add(Pair.of(meeting.getEndTime(), -1));
+        }
+        Collections.sort(events, (a, b) -> {
+            int cmp = a.getKey().compareTo(b.getKey());
+            if (cmp == 0) {
+                cmp = a.getValue().compareTo(b.getValue());
+            }
+            return cmp;
+        });
+        int maxMeetings = 0;
+        int currentMeetings = 0;
+        for (Pair<LocalTime, Integer> event : events) {
+            currentMeetings += event.getValue();
+            maxMeetings = Math.max(maxMeetings, currentMeetings);
+        }
+        return maxMeetings+1;
+//        Collections.sort(calendar, (Meeting m1, Meeting m2) -> m1.getStartTime().compareTo(m2.getStartTime()));
+//        Meeting m2=calendar.get(0);
+//        LocalTime currentEndTime =m2.getEndTime();
 //        int maxMeetings = 0;
-//        int currentMeetings = 0;
-//        for (Pair<LocalTime, Integer> event : events) {
-//            currentMeetings += event.getValue();
-//            maxMeetings = Math.max(maxMeetings, currentMeetings);
+//        for (int i = 0; i < calendar.size(); i++) {
+//            Meeting currentMeeting = calendar.get(i);
+//            if (currentMeeting.getStartTime().compareTo(currentEndTime) > 0) {
+//                maxMeetings++;
+//                currentEndTime = currentMeeting.getEndTime();
+//            }
 //        }
-        return 2;//maxMeetings;
+//        return maxMeetings;
     }
 }
